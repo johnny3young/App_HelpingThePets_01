90a10ee6f632d42bd4831a820670af31e34c8989
Black3 <youngerblack@gmail.com>
2019-04-21 22:23:26 -0500

    Modificacion desde mi Apto

--- app/src/main/java/com/example/app_helpingthepets_01/LoginGuestActivity.java
+++ app/src/main/java/com/example/app_helpingthepets_01/LoginGuestActivity.java
@@ -17,20 +17,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginGuestActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginGuestActivity extends AppCompatActivity{
 
     Button btnregistrar, btnregresar;
     EditText edtguestemail, edtguestpass;
     ProgressBar progressBar;
 
-    private FirebaseAuth firebaseAuth;
+    //private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_guest);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        // firebaseAuth = FirebaseAuth.getInstance();
 
         edtguestemail = findViewById(R.id.edtguestemail);
         edtguestpass = findViewById(R.id.edtguestpass);
@@ -39,57 +39,57 @@ public class LoginGuestActivity extends AppCompatActivity implements View.OnClic
 
         progressBar = new ProgressBar(this);
 
-        btnregistrar.setOnClickListener((View.OnClickListener) this);
-
-        btnregresar.setOnClickListener(new View.OnClickListener() {
+       btnregresar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+           public void onClick(View v) {
                 Intent myIntent = new Intent(LoginGuestActivity.this, LoginActivity.class);
-                startActivity(myIntent);
-                finish();
+               startActivity(myIntent);
+               finish();
             }
         });
     }
 
-    private void registrarUsuario(){
-        String email = edtguestemail.getText().toString().trim();
-        String password = edtguestpass.getText().toString().trim();
+    // btnregistrar.setOnClickListener((View.OnClickListener) this);
 
-        //Verificamos que las cajas de texto no estén vacías
-        if (TextUtils.isEmpty(email)){ // (precio.equals(""))
-            Toast.makeText(this,"Se debe ingresar un email ", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Se debe ingresar una contraseña ", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        progressBar.setProgress(Integer.parseInt("Realizando el registro......."));
-        progressBar.isShown();
-
-
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                //Cheking if success
-                if (task.isSuccessful()){
-                    Toast.makeText(LoginGuestActivity.this, "Se ha registrado el email: ", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(LoginGuestActivity.this, "Nose pudo registrar el usuario", Toast.LENGTH_SHORT).show();
-                }
-                progressBar.isShown();
-            }
-
-
-        });
-
-    }
-
-    @Override
-    public void onClick (View v2) {
-        //Invocamos al método
-        registrarUsuario();
-    }
+//    private void registrarUsuario(){
+//        String email = edtguestemail.getText().toString().trim();
+//        String password = edtguestpass.getText().toString().trim();
+//
+//        //Verificamos que las cajas de texto no estén vacías
+//        if (TextUtils.isEmpty(email)){ // (precio.equals(""))
+//            Toast.makeText(this,"Se debe ingresar un email ", Toast.LENGTH_LONG).show();
+//            return;
+//        }
+//        if (TextUtils.isEmpty(password)) {
+//            Toast.makeText(this, "Se debe ingresar una contraseña ", Toast.LENGTH_LONG).show();
+//            return;
+//        }
+//
+//        progressBar.setProgress(Integer.parseInt("Realizando el registro......."));
+//        progressBar.isShown();
+//
+//
+//        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                //Cheking if success
+//                if (task.isSuccessful()){
+//                    Toast.makeText(LoginGuestActivity.this, "Se ha registrado el email: ", Toast.LENGTH_SHORT).show();
+//                }else {
+//                    Toast.makeText(LoginGuestActivity.this, "Nose pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+//                }
+//                progressBar.isShown();
+//            }
+//
+//
+//        });
+//
+//    }
+//
+//    @Override
+//    public void onClick (View v2) {
+//        //Invocamos al método
+//        registrarUsuario();
+//    }
 
 }
