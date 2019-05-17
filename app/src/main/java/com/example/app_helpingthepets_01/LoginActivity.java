078a0b4277e5c64729d1335d6b238d5641f93dc7
johnny3young <youngerblack@gmail.com>
2019-05-17 11:34:15 -0500

    Se remueven cambios del LoginButton de Facebook porque no quedó funcionando

--- app/src/main/java/com/example/app_helpingthepets_01/LoginActivity.java
+++ app/src/main/java/com/example/app_helpingthepets_01/LoginActivity.java
@@ -6,57 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
 public class LoginActivity extends AppCompatActivity {
 
     Button btnguest, btngmail, btnfacebook;
-    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        CallbackManager callbackManager = CallbackManager.Factory.create();
 
         btnguest = findViewById(R.id.btnguest);
         btngmail = findViewById(R.id.btngmail);
         btnfacebook = findViewById(R.id.btnfacebook);
-        loginButton = findViewById(R.id.login_button);
 
         btnguest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(LoginActivity.this,LoginGuestActivity.class);
                 startActivity(myIntent);
-            }
-        });
-
-        loginButton.setReadPermissions("email");
-        // If using in a fragment
-        loginButton.setFragment(this);
-
-        // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
             }
         });
     }
