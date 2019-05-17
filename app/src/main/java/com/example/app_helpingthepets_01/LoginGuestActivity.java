380d35cae64ab6a2ea02f6e407dba042a60e70c3
johnny3young <youngerblack@gmail.com>
2019-05-17 12:56:47 -0500

    Se remueven cambios del LoginButton de Facebook porque no quedó funcionando

--- app/src/main/java/com/example/app_helpingthepets_01/LoginGuestActivity.java
+++ app/src/main/java/com/example/app_helpingthepets_01/LoginGuestActivity.java
@@ -1,22 +1,14 @@
 package com.example.app_helpingthepets_01;
 
-import android.content.DialogInterface;
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.facebook.CallbackManager;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginGuestActivity extends AppCompatActivity{
 
@@ -38,7 +30,7 @@ public class LoginGuestActivity extends AppCompatActivity{
         btnregistrar = findViewById(R.id.btnregristrar);
         btnregresar = findViewById(R.id.btnregresar);
 
-        progressBar = new ProgressBar(this);
+        //progressBar = new ProgressBar(this);
 
        btnregresar.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/app_helpingthepets_01/MainActivity.java
+++ app/src/main/java/com/example/app_helpingthepets_01/MainActivity.java
@@ -4,15 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
 
         new Timer().schedule(new TimerTask() {
             @Override
